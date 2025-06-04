2004cb2b5a48cbe900c5a2221137d73aaf8db143
leonardosarquiz <leonardosarquiz14@gmail.com>
2025-06-03 22:51:54 -0300

    add config

---
+++ shopee-dashboard-backend/src/app.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+export default app;
